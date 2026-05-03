6108310cdafe53335ac1583b19a5169d5d57a1ca
EngAhmedElshaer <aboabdolaaa55@gmail.com>
2026-05-03 10:40:56 +0200

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,5 +1,33 @@
 package program1;
 
 public class Teacher extends Person{
+
+
+// Fields    
+    String name;
+    int age;
+    double salary;
+    String subject;
+    String phone;
+
+
+// Constructor
+    public Teacher(String name, int age, double salary, String subject , String phone ) {
+        this.name =name;
+                this.age =age;
+                        this.salary =salary;
+                        this.subject =subject;
+                        this.phone = phone;
+    }
     
+    
+    // Method (print)
+    public void display(int index){
+        
+        System.out.printf("No. %-3d | Name: %-15s | Age: %-4d | Salary: %-8.1f | Subject: %-12s Phone: %-12s %n" , index , name , age , salary , subject , phone);
 }
+        
+        
+
+    
+
